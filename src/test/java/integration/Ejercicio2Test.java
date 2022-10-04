a9a902116cbb69259bab3a736a5c4be743c46760
Casado Horacio <hcasado@santandertecnologia.com.ar>
2022-10-04 20:22:54 -0300

    refactor ejercicio2 tests

--- src/test/java/integration/Ejercicio2Test.java
+++ src/test/java/integration/Ejercicio2Test.java
@@ -21,12 +21,13 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class RepositoriesTest {
+public class Ejercicio2Test {
     private static InscriptionRepository inscriptionRepository;
     private static StudentRepository studentRepository;
     private static CareerRepository careerRepository;
     private static Student cio, lu, agus;
     private static Career ingenieria, tudai, fisica;
+    private static Inscription cioInscription, luInscription, agusInscription;
 
     @BeforeClass
     public static void instantiateEntityManagerAndRepositories(){
@@ -42,7 +43,7 @@ public class RepositoriesTest {
         Timestamp birthdate = createTimestamp("31/03/1995");
         cio = new Student(1234,"Cio","Casado",birthdate,"male","mar del plata");
         lu = new Student(3231,"Lu","Blanco",birthdate,"female", "tandil");
-        agus = new Student(2342,"Agus","Bedrossian",birthdate,"female","tandil");
+        agus = new Student(2342,"Agus","Bedrossian",birthdate,"female","balcarce");
         ingenieria = new Career("Ingeniería de Sistemas");
         tudai = new Career("Tecnicatura Universitaria de Desarrollo de Aplicaciones Informaticas");
         fisica = new Career("Licenciatura en Física");
@@ -79,31 +80,33 @@ public class RepositoriesTest {
         careerRepository.save(fisica);
     }
 
-    /**
-     * a) dar de alta un estudiante
-     * d) recuperar un estudiante, en base a su número de libreta universitaria
-     */
-    @Test
-    public void saveAndFindStudentsByBookNumberTest(){
-        Timestamp birthdate = createTimestamp("31/03/1995");
-        Student cio = new Student(1234,"Cio","Casado",birthdate,"male","mar del plata");
-        studentRepository.save(cio);
-        Student studentFound = studentRepository.findByBookNumber(cio.getBookNumber());
-        assertEquals(studentFound,cio);
+    private static void createInscriptions(){
+        createStudents();
+        createCareers();
+        cioInscription = new Inscription(cio,ingenieria);
+        luInscription = new Inscription(lu,tudai);
+        agusInscription = new Inscription(agus,tudai);
+        inscriptionRepository.save(cioInscription);
+        inscriptionRepository.save(luInscription);
+        inscriptionRepository.save(agusInscription);
     }
 
     /**
-     * b) matricular un estudiante en una carrera
+     * 2) a) dar de alta un estudiante
      */
     @Test
-    public void saveInscriptionTest(){
-        Timestamp birthdate = createTimestamp("31/03/1995");
-        Student cio = new Student(1234,"Cio","Casado",birthdate,"male","mar del plata");
+    public void darDeAltaEstudianteTest(){
+        Student s = studentRepository.save(cio);
+        assertTrue(s.getBookNumber() != -1);
+    }
+
+    /**
+     * 2) b) matricular un estudiante en una carrera
+     */
+    @Test
+    public void matricularEstudianteTest(){
         studentRepository.save(cio);
-
-        Career ingenieria = new Career("Ingeniería de Sistemas");
         careerRepository.save(ingenieria);
-
         Inscription cioInscription = new Inscription(cio,ingenieria);
         inscriptionRepository.save(cioInscription);
         Inscription inscriptionFound = inscriptionRepository.findByStudentAndCareer(cio,ingenieria);
@@ -111,57 +114,55 @@ public class RepositoriesTest {
     }
 
     /**
-     * c)recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple.
+     * 2) c) recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple.
      */
     @Test
-    public void findAllStudentsTest(){
+    public void recuperarTodosLosEstudiantesTest(){
         createStudents();
         List<Student> studentsFound = studentRepository.findAllSortedByName();
         assertTrue(studentsFound.get(0).equals(agus) && studentsFound.get(1).equals(cio) && studentsFound.get(2).equals(lu));
     }
 
     /**
-     * e) recuperar todos los estudiantes, en base a su género.
+     * 2) d) recuperar un estudiante, en base a su número de libreta universitaria
      */
     @Test
-    public void findStudentsByGenderTest(){
+    public void recuperarEstudianteSegunLibretaTest(){
+        studentRepository.save(cio);
+        Student studentFound = studentRepository.findByBookNumber(cio.getBookNumber());
+        assertEquals(studentFound,cio);
+    }
+
+    /**
+     * 2) e) recuperar todos los estudiantes, en base a su género.
+     */
+    @Test
+    public void RecuperarEstudiantesPorGeneroTest(){
         createStudents();
         List<Student> studentsFound = studentRepository.findByGender("female");
-
         assertTrue(!studentsFound.contains(cio) && studentsFound.contains(lu) && studentsFound.contains(agus));
     }
 
     /**
-     * f) recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.
+     * 2) f) recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.
      */
     @Test
-    public void findCareersWithEnrolledStudentsTest(){
-        createStudents();
-        createCareers();
-
-        Timestamp inscriptionDate = createTimestamp("01/01/2022");
-        Inscription cioInscription = new Inscription(cio,ingenieria);
-        Inscription luInscription = new Inscription(lu,tudai);
-        Inscription agusInscription = new Inscription(agus,tudai);
-
-        inscriptionRepository.save(cioInscription);
-        inscriptionRepository.save(luInscription);
-        inscriptionRepository.save(agusInscription);
+    public void recuperarCarrerasConInscriptosTest(){
+        createInscriptions();
 
         List<Career> careersFound = careerRepository.findWithEnrolledStudents();
-        assertTrue(!careersFound.contains(fisica) && careersFound.contains(ingenieria) && careersFound.contains(tudai));
+        assertTrue(careersFound.get(0).equals(tudai) && careersFound.get(1).equals(ingenieria)  && !careersFound.contains(fisica));
     }
 
     /**
-     * g) recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia.
+     * 2) g) recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia.
      */
     @Test
-    public void findStudentsByCareerAndCity() {
-        createStudents();
-        createCareers();
-
-        List<Student> studentsFound = studentRepository.findByCareerAndCity(ingenieria,"Tandil");
-        assertTrue(!studentsFound.contains(cio) && studentsFound.contains(lu) && studentsFound.contains(agus));
+    public void recuperarEstudiantesPorCarreraYCiudadTest() {
+        createInscriptions();
+        List<Student> studentsFound = studentRepository.findByCareerAndCity(tudai,"tandil");
+        // cio: de ingenieria y tandil, agus de tudai y balcarce y lu de tudai y tandil
+        assertTrue(studentsFound.contains(lu) && !studentsFound.contains(agus) && !studentsFound.contains(cio));
     }
 
 }
